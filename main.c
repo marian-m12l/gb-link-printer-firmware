9586785ea3aac6544fb916f445956ef28064418c
marian <marian.muller@serli.com>
2021-07-30 21:26:43 +0200

    cleanup + printer state-machine

--- main.c
+++ main.c
@@ -36,11 +36,7 @@
 #include "hardware/pio.h"
 
 
-#ifdef SECONDARY_MODE
-  #include "pio/pio_secondary.h"
-#else
-  #include "pio/pio_spi.h"
-#endif
+#include "pio/pio_secondary.h"
 const uint SI_PIN = 3;
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
@@ -53,11 +49,7 @@ const uint SI_PIN = 3;
  */
 enum  {
   BLINK_NOT_MOUNTED = 250,
-#ifdef SECONDARY_MODE
   BLINK_MOUNTED     = 500,
-#else
-  BLINK_MOUNTED     = 1000,
-#endif
   BLINK_SUSPENDED   = 2500,
 
   BLINK_ALWAYS_ON   = UINT32_MAX,
@@ -66,11 +58,7 @@ enum  {
 
 static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
 
-#ifdef SECONDARY_MODE
-#define URL  "localhost:3001"
-#else
 #define URL  "localhost:3000"
-#endif
 
 const tusb_desc_webusb_url_t desc_url =
 {
@@ -86,23 +74,15 @@ static bool web_serial_connected = false;
 void led_blinking_task(void);
 void cdc_task(void);
 void webserial_task(void);
-#ifdef SECONDARY_MODE
 void secondary_task(void);
-#endif
+void upload_task(void);
 
 /*------------- MAIN -------------*/
 
-#ifdef SECONDARY_MODE
-  pio_secondary_inst_t secondary = {
-          .pio = pio1,
-          .sm = 0
-  };
-#else
-  pio_spi_inst_t spi = {
-          .pio = pio1,
-          .sm = 0
-  };
-#endif
+pio_secondary_inst_t secondary = {
+        .pio = pio1,
+        .sm = 0
+};
 
 
 
@@ -114,14 +94,8 @@ const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 int main(void)
 {
-  #ifdef SECONDARY_MODE
-    uint secondary_prog_offs = pio_add_program(secondary.pio, &secondary_program);
-    secondary_program_init(secondary.pio, secondary.sm, secondary_prog_offs, PIN_SCK, PIN_SIN, PIN_SOUT);
-  #else
-    uint cpha1_prog_offs = pio_add_program(spi.pio, &spi_cpha1_program);
-    pio_spi_init(spi.pio, spi.sm, cpha1_prog_offs, 8, 4058.838, 1, 1, PIN_SCK, PIN_SOUT, PIN_SIN);
-  #endif
-
+  uint secondary_prog_offs = pio_add_program(secondary.pio, &secondary_program);
+  secondary_program_init(secondary.pio, secondary.sm, secondary_prog_offs, PIN_SCK, PIN_SIN, PIN_SOUT);
 
 
   gpio_init(LED_PIN);
@@ -135,26 +109,8 @@ int main(void)
     cdc_task();
     webserial_task();
     led_blinking_task();
-    #ifdef SECONDARY_MODE
-      secondary_task();                                                                                                                           
-    #endif
-  }
-
-  return 0;
-}
-
-int oldmain(void)
-{
-  board_init();
-
-  tusb_init();
-
-  while (1)
-  {
-    tud_task(); // tinyusb device task
-    cdc_task();
-    webserial_task();
-    led_blinking_task();
+    secondary_task();
+    upload_task();
   }
 
   return 0;
@@ -287,20 +243,8 @@ void webserial_task(void)
     {
       uint8_t buf[1];
       uint32_t count = tud_vendor_read(buf, sizeof(buf));
-      if(count) {
-        // pprintf("Sending: %02x", buf[0]);
 
-        #ifdef SECONDARY_MODE
-          pio_secondary_write8_blocking(&secondary, buf, count); // Stores in outgoing FIFO to be sent whenever the primary device issues clock ticks
-        #else
-          unsigned char rx;
-          pio_spi_write8_read8_blocking(&spi, buf, &rx, 1);
-          echo_all(&rx, 1);
-        #endif
-
-      }
-      // echo back to both web serial and cdc
-      // echo_all(buf, count);
+      // No-op
     }
   }
 }
@@ -319,25 +263,8 @@ void cdc_task(void)
       
       uint8_t buf[1];
       uint32_t count = tud_vendor_read(buf, sizeof(buf));
-      if(count) {
 
-        #ifdef SECONDARY_MODE
-          pio_secondary_write8_blocking(&secondary, buf, count); // Stores in outgoing FIFO to be sent whenever the primary device issues clock ticks
-        #else
-          unsigned char rx;
-          pio_spi_write8_read8_blocking(&spi, buf, &rx, 1);
-          echo_all(&rx, 1);
-        #endif
-      }
-      // echo back to both web serial and cdc
-      // echo_all(buf, count);
-    
-      // uint8_t buf[64];
-
-      // uint32_t count = tud_cdc_read(buf, sizeof(buf));
-
-      // // echo back to both web serial and cdc
-      // echo_all(buf, count);
+      // No-op
     }
   }
 }
@@ -379,19 +306,148 @@ void led_blinking_task(void)
 }
 
 
-#ifdef SECONDARY_MODE
-  void secondary_task(void)
-  {
-    // Is data available on PIO ?
-    if (pio_secondary_available(&secondary)) {
-      // Read byte
-      //uint8_t rx = pio_secondary_read8(&secondary); // TODO Use buffer as argument instead of return value ???
-      uint8_t rx;
-      pio_secondary_read8(&secondary, &rx, 1);
-      //pio_secondary_read8_blocking(&secondary, &rx, 1);
-  
-      // Send byte to both web serial and cdc
-      echo_all(&rx, 1);
+
+// GB Printer state-machine
+enum {
+  PRINTER_STATE_IDLE = 0,
+  PRINTER_STATE_MAGIC_BYTE_1,
+  PRINTER_STATE_MAGIC_BYTE_2,
+  PRINTER_STATE_COMMAND_BYTE,
+  PRINTER_STATE_COMPRESSION_BYTE,
+  PRINTER_STATE_LENGTH_BYTE_1,
+  PRINTER_STATE_LENGTH_BYTE_2,
+  PRINTER_STATE_DATA_BYTES,
+  PRINTER_STATE_DATA_BYTES_DONE,
+  PRINTER_STATE_CRC_BYTE_1,
+  PRINTER_STATE_CRC_BYTE_2,
+  PRINTER_STATE_KEEPALIVE_BYTE,
+  PRINTER_STATE_STATUS_BYTE,
+};
+uint8_t state = PRINTER_STATE_IDLE;
+uint8_t commandCode = 0x00;
+uint16_t dataLength = 0;
+uint16_t dataCounter = 0;
+uint16_t dataOffset = 0;
+uint8_t data[640*9];
+uint8_t printParams[4];
+uint8_t next = 0x00;
+uint8_t printerStatus = 0x00;
+bool dataLatch = false;
+
+void secondary_task(void)
+{
+  // Is data available on PIO ?
+  if (pio_secondary_available(&secondary)) {
+    // Read byte
+    uint8_t rx;
+    pio_secondary_read8(&secondary, &rx, 1);
+
+    switch (state) {
+    case PRINTER_STATE_IDLE:
+    case PRINTER_STATE_STATUS_BYTE:
+      if (rx == 0x88) state = PRINTER_STATE_MAGIC_BYTE_1;
+      break;
+    case PRINTER_STATE_MAGIC_BYTE_1:
+      if (rx == 0x33) state = PRINTER_STATE_MAGIC_BYTE_2;
+      else state = PRINTER_STATE_IDLE;
+      break;
+    case PRINTER_STATE_MAGIC_BYTE_2:
+      if (rx == 0x01 || rx == 0x02 || rx == 0x04 || rx == 0x0f) {
+        commandCode = rx;
+        state = PRINTER_STATE_COMMAND_BYTE;
+      } else state = PRINTER_STATE_IDLE;
+      break;
+    case PRINTER_STATE_COMMAND_BYTE:
+      if (rx == 0x00 || rx == 0x01) {
+        // TODO Handle RLE compression?
+        state = PRINTER_STATE_COMPRESSION_BYTE;
+      } else state = PRINTER_STATE_IDLE;
+      break;
+    case PRINTER_STATE_COMPRESSION_BYTE:
+      dataLength = rx;
+      state = PRINTER_STATE_LENGTH_BYTE_1;
+      break;
+    case PRINTER_STATE_LENGTH_BYTE_1:
+      dataLength = ((uint16_t)rx << 8) | dataLength;
+      if (dataLength > 0) state = PRINTER_STATE_LENGTH_BYTE_2;
+      else state = PRINTER_STATE_DATA_BYTES_DONE;
+      break;
+    case PRINTER_STATE_LENGTH_BYTE_2:
+    case PRINTER_STATE_DATA_BYTES:
+      if (commandCode == 0x04) {
+        data[dataOffset+dataCounter] = rx;
+      } else if (commandCode == 0x02) {
+        printParams[dataCounter] = rx;
+      }
+      dataCounter++;
+      if (dataCounter == dataLength) state = PRINTER_STATE_DATA_BYTES_DONE;
+      else state = PRINTER_STATE_DATA_BYTES;
+      break;
+    case PRINTER_STATE_DATA_BYTES_DONE:
+      // TODO Read and check CRC?
+      state = PRINTER_STATE_CRC_BYTE_1;
+      break;
+    case PRINTER_STATE_CRC_BYTE_1:
+      // TODO Read and check CRC?
+      state = PRINTER_STATE_CRC_BYTE_2;
+      next = 0x81;  // Write 0x81 for keepalive
+      break;
+    case PRINTER_STATE_CRC_BYTE_2:
+      state = PRINTER_STATE_KEEPALIVE_BYTE;
+      next = printerStatus; // Write current printer status
+      // Update printer status
+      if (printerStatus == 0x00 && commandCode == 0x01) printerStatus = 0x08; // Status: ready to receive data
+      if (printerStatus == 0x08 && commandCode == 0x02) printerStatus = 0x06; // Status: printing
+      if (printerStatus == 0x06 && commandCode == 0x00) printerStatus = 0x00; // Status: printing done FIXME Should be 0x04?
+      break;
+    case PRINTER_STATE_KEEPALIVE_BYTE:
+      next = 0x00;
+      if (commandCode == 0x02) {
+        // Print command is complete: latch data to be sent over usb
+        dataLatch = true;
+        // TODO Also send print params?
+      } else if (commandCode == 0x04) {
+        dataOffset += dataCounter;
+      }
+      // Get ready to receive next command packet
+      state = PRINTER_STATE_STATUS_BYTE;
+      commandCode = 0x00;
+      dataLength = 0;
+      dataCounter = 0;
+      break;
+    default:
+      break;
+    }
+
+    if (state != PRINTER_STATE_IDLE) {
+      pio_secondary_write8_blocking(&secondary, &next, 1); // Stores in outgoing FIFO to be sent whenever the primary device issues clock ticks
+    }
+
+    // Send byte to both web serial and cdc
+    //echo_all(&rx, 1); // Only for debug
+  }
+}
+
+
+void upload_task(void)
+{
+  // Is data latched?
+  if (dataLatch) {
+    uint32_t remaining = dataOffset - dataCounter;
+    if (remaining > 0) {
+      uint32_t available = tud_vendor_write_available();
+      if (available > 0) {
+        uint8_t chunk_size = (available < remaining) ? available : remaining;
+        echo_all(&data[dataCounter], chunk_size);
+        dataCounter += chunk_size;
+      }
+    } else {
+      // TODO Also send print params?
+      // Reset printer state
+      dataOffset = 0;
+      printerStatus = 0x00;
+      //memset(data, 0, 640*9);
+      dataLatch = false;
     }
   }
-#endif
+}
